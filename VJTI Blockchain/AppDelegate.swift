30ed0e8efef8aa033ab0443a49ebad5d51aed704
Ameya Daddikar <ameyadaddikar@gmail.com>
2019-05-24 15:09:54 +0530

    Added link between signup and main storyboards

--- VJTI Blockchain/AppDelegate.swift
+++ VJTI Blockchain/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let userLoggedIn = true
+        let userLoggedIn = false
         
         if userLoggedIn {
             let mainStoryBoard   = UIStoryboard(name: "Main", bundle: nil);
