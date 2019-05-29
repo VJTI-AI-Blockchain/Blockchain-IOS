10101019bd89c51d2819ab65b3afc1a276f34381
Ameya Daddikar <ameyadaddikar@gmail.com>
2019-05-29 23:38:19 +0530

    Added pod and other assets

---
+++ Pods/Headers/Public/QRCode/QRCode-umbrella.h
@@ -0,0 +1,1 @@
+../../../Target Support Files/QRCode/QRCode-umbrella.h
---
+++ Pods/Target Support Files/Pods-VJTI Blockchain/Pods-VJTI Blockchain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VJTI_Blockchain : NSObject
+@end
+@implementation PodsDummy_Pods_VJTI_Blockchain
+@end
